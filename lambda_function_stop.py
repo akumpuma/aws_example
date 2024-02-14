991c195159a5c5954a83e7c453e6f8702982fce0
akumpuma <arto.kumpumaki@gmail.com>
2024-02-14 09:38:25 +0200

    Add files via upload

---
+++ lambda_function_stop.py
@@ -0,0 +1,48 @@
+import boto3
+import logging
+
+
+#setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#define the connection
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    # Use the filter() method of the instances collection to retrieve
+    # all running EC2 instances.
+    filters = [{
+            'Name': 'tag:Project',
+            'Values': ['nokia-ncc-ci']
+        }
+        ,
+        {
+            'Name': 'tag:ClusterNumber',
+            'Values': ['one']
+        }
+        ,
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['running']
+        }
+    ]
+    
+    #filter the instances
+    instances = ec2.instances.filter(Filters=filters)
+    
+    #locate all running instances
+    RunningInstances = [instance.id for instance in instances]
+    print (RunningInstances)
+    
+    #print the instances for logging purposes
+    #print RunningInstances
+    
+    #make sure there are actually instances to shut down. 
+    if len(RunningInstances) > 0:
+        #perform the shutdown
+        shuttingDown = ec2.instances.filter(InstanceIds=RunningInstances).stop()
+        print (shuttingDown)
+    else:
+        print ("Nothing to stop")
+
